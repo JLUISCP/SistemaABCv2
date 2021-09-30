a72b230899cfa5fb5fd059be5baac9bc1a1acafe
Dan <olveradan@hotmail.com>
2021-09-30 14:46:53 -0500

    Creacion de la pantalla principal del sistema

--- src/main/java/Vistas/Main.java
+++ src/main/java/Vistas/Main.java
@@ -44,13 +44,7 @@ public class Main {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                
-                new VistaGestion().setVisible(true);
-                try {
-                    new VistaGestionHistoriaAcademica().setVisible(true);
-                } catch (SQLException ex) {
-                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                new VistaPrincipal().setVisible(true);
             }
         });
     }
